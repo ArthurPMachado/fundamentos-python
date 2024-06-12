c597a5585472b7fa877d88c8a130417bc8d2982d
Arthur Machado <machadoparthur1@gmail.com>
2024-06-12 15:51:54 -0300

    feat: explore python syntax

---
+++ syntax.py
@@ -0,0 +1,14 @@
+# This is my first comment
+"""This is my first 
+   multiline comment  
+"""
+
+name = "Arthur" + \
+" Machado"
+
+age = 25
+
+if age < 18:
+  print("Minor")
+else:
+  print("Legal age")
