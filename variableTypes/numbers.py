19146a0d3fe2e5dbd95dd88d1840b69fbbbf352b
Arthur Machado <machadoparthur1@gmail.com>
2024-07-03 22:24:19 -0300

    feat: explore python numbers syntax

---
+++ variableTypes/numbers.py
@@ -0,0 +1,21 @@
+integer_number = 10
+float_number = 3.14
+
+print("Variable type integer: ", type(integer_number))
+print("Variable type float: ", type(float_number))
+
+add = 2 + 3
+sub = 2 - 3
+mul = 2 * 3
+div = 5 / 3
+module = 10 % 2
+int_div = 5 // 3
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+print("Module: ", module)
+print("Integer division: ", int_div)
+print("Float to integer: ", int(div))
+print("Integer to float: ", float(add))
