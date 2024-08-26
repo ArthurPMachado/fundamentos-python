3b636704cd7c4642595c41258df3800597182d68
Arthur Machado <machadoparthur1@gmail.com>
2024-08-26 11:25:15 -0300

    feat: explore python list syntax

---
+++ variableTypes/list.py
@@ -0,0 +1,10 @@
+# Declaration
+
+list = [1, 2, 3, 4, 5, "test", True, False]
+
+print("List: ", list)
+print("List at index 0: ", list[0])
+print("List at index 5: ", list[5])
+print("Sliced list[1:6]: ", list[1:6]) # Output: [2, 3, 4, 5, "test"]
+print("Sliced list[:5]: ", list[:5]) # Output: [1, 2, 3, 4, 5]
+print("Sliced list[5:]: ", list[5:]) # Output: ["test", True, False]
