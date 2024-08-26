2398ffea1206fcb4437161468335a76af5c8a4b6
Arthur Machado <machadoparthur1@gmail.com>
2024-08-26 10:55:53 -0300

    feat: explore python booleans

---
+++ variableTypes/booleans.py
@@ -0,0 +1,24 @@
+# Conditions (Booleans)
+if True:
+  print("If statement is true")
+else:
+  print("If statement is false")
+
+# Logical operators: and or
+
+# AND
+if True and True:
+  print("Both statements are true")
+
+if True and False:
+  print('True and false does not execute')
+
+if False and False:
+  print('False and false does not execute')
+
+# OR
+if True or False:
+  print("True or false does execute")
+
+if False or False:
+  print('False or false does not execute')
