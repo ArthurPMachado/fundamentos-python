6e4f661f326e70ab0c468338c816a0ede547e579
Arthur Machado <machadoparthur1@gmail.com>
2024-08-27 12:54:10 -0300

    feat: explore python input

---
+++ variableTypes/data-input.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age:"))
+if age >= 18:
+  print("Legal age")
+
+if age == 18:
+  print('You are 18 years old')
+
+if age < 18:
+  print('Minor')
+
+if age != 10:
+  print('You are not 10 years old')
