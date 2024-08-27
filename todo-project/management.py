7ddb31a21bd78885ce23580b33ccde5b7a54870c
Arthur Machado <machadoparthur1@gmail.com>
2024-08-27 18:50:08 -0300

    feat: start todo list project

---
+++ todo-project/management.py
@@ -0,0 +1,15 @@
+while True:
+  print('\n Todo List Management Menu:')
+  print('1. Adding new task')
+  print('2. List tasks')
+  print('3. Update task')
+  print('4. Finish task')
+  print('5. Delete completed tasks')
+  print('6. Exit program')
+
+  option = int(input('Choose an option: '))
+
+  if option == 6:
+    break
+
+print('Finished program')
