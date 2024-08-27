41d41206c05a740f92bb942993319580e27a5472
Arthur Machado <machadoparthur1@gmail.com>
2024-08-26 22:08:18 -0300

    feat: explore python dictionary syntax

---
+++ variableTypes/dictonaries.py
@@ -0,0 +1,13 @@
+person = {
+  "name": "Arthur",
+  "age": 25,
+  "city": "São Paulo",
+}
+
+print('Person: ', person)
+print('Person name: ', person['name'])
+print('Person age: ', person['age'])
+print('Person city: ', person['city'])
+
+person['surname'] = 'Pereira Machado'
+print('Person surname: ', person['surname'])
