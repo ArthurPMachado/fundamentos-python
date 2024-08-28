53f8212fe7fb2c0d386bcf19469aafe7815f66ce
Arthur Machado <machadoparthur1@gmail.com>
2024-08-28 18:16:15 -0300

    feat: create add task feature

--- todo-project/management.py
+++ todo-project/management.py
@@ -1,3 +1,14 @@
+def add_task(tasks: list, task_name):
+  task = {
+    "task": task_name,
+    "completed": False
+  }
+
+  tasks.append(task)
+  print(f'Tasks {task_name} was added successfully')
+  return
+
+tasks = []
 while True:
   print('\n Todo List Management Menu:')
   print('1. Adding new task')
@@ -9,7 +20,10 @@ while True:
 
   option = int(input('Choose an option: '))
 
-  if option == 6:
+  if option == 1:
+    task_name = input('Enter task name:')
+    add_task(tasks, task_name)
+  elif option == 6:
     break
 
 print('Finished program')
